049fd658d8e1634b5cd7df79aec81284199e807d
Samarth Singh <133684928+Samarthsingh1995@users.noreply.github.com>
2023-05-16 11:03:18 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,16 @@
+create database coachx
+
+use coachx;
+
+create table emp
+( empid int,
+   empname varchar(10),
+   dept varchar(10)
+   );
+
+   select * from emp;
+
+   insert into emp values(1,'ABC','CS');
+   insert into emp values(2,'XYZ','EC');
+   insert into emp values(3,'QWE','MEchanical');
+
